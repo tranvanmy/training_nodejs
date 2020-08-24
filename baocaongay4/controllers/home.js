0e96cc697ea5ee312fd25bec69182af670e7124f
minhnc62 <you@example.com>
2020-08-24 17:42:06 +0700

    update

--- baocaongay4/controllers/home.js
+++
@@ -1,9 +0,0 @@
-const BlogPost = require('../models/BlogPost.js')
-module.exports = (req, res) => {
-    BlogPost.find({}, function (error, posts) {
-        console.log(posts);
-        res.render('index', {
-            blogposts: posts
- });
- })
-}
